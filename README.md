dbdfd1623ee5cfd21c35b490b724f45406d5e4d2
Nicholas Gimbel <56981761+ngimb64@users.noreply.github.com>
2022-08-06 19:25:41 -0400

    Update - 08/06/22

--- README.md
+++ README.md
@@ -1,29 +1,47 @@
 ## Shellcode Generator
 ![alt text](https://github.com/ngimb64/Shellcode-Generator/blob/main/ShellcodeGen.png?raw=true)
 
+&#9745;&#65039; Bandit verified<br>
+&#9745;&#65039; Synk verified<br>
+&#9745;&#65039; Pylint verified 10/10
+
 ## Prereqs
 This program was designed for Python3 on Linux systems with objdump utility.
 The modules used should already be included in the Python3 default installation.
 
 ## Purpose
-The purpose is to run objdump on selected file, grab bytes, & format as shellcode.
-Ideally this program can be added to environment executables/path as a handy terminal utility.
+The purpose is to run objdump on selected file, grab output bytes, & format as shellcode.
 
 ## Installation
 - Run the setup.py script to build a virtual environment and install all external packages in the created venv.
 
-> Example:<br>
-> python3 setup.py "venv name"
+> Example: `python3 setup.py venv`
 
 - Once virtual env is built traverse to the (Scripts-Windows or bin-Linux) directory in the environment folder just created.
-- In the bin directory, run the command `source activate` to activate the virtual environment.
+- For Windows in the Scripts directory, for execute the `./activate` script to activate the virtual environment.
+- For Linux in the bin directory, run the command `source activate` to activate the virtual environment.
 
 ## How it works
-> Example:<br>
-> python3 shellcodeGen.py &lt;executable\_name&gt;
+> Example: `python3 shellcodeGen.py &lt;executable&gt;`
 
 - If arg is not passed, the user is prompted for input
 - Selected executable is run in objdump, the output in intel syntax is redirected to a temporary text file
 - The text file is then iterated over line by line; grabbing bytes, stripping whitespace, & formatting it as shellcode
 - The shellcode formatted for the current line will be appended to a variable
 - Once all iterations are complete the temp file is deleted and final shellcode result is provided
+
+## Function Layout
+-- shellcode_gen.py --
+> objdump_run &nbsp;-&nbsp; Takes the passed in binary executable, runs objdump utility, and writes the output to file.
+
+> print_err &nbsp;-&nbsp; Prints a timed error message via stderr.
+
+> prompt_user &nbsp;-&nbsp; Prompt the user for input, handle errors accordingly.
+
+> main &nbsp;-&nbsp; Take input file, run in objdump, and parse output into shellcode.
+
+## Exit Codes
+> 0 - Successful operation
+> 1 - Error occurred during parsing of args in startup
+> 2 - Error occurred writing objdump output to file
+> 3 - Error occurred reading objdump output for parsing shellcode
---
+++ shellcode_gen.py
@@ -0,0 +1,148 @@
+""" Built-in modules """
+import os
+import re
+import shlex
+import sys
+import time
+from subprocess import Popen, TimeoutExpired
+
+
+def objdump_run(filename: str, tmp_file: str):
+    """
+    Takes the passed in binary executable, runs objdump utility, and writes the output to file.
+
+    :param filename:  The file to be run through the objdump utility.
+    :param tmp_file:  The temp file where the objdump output will be saved to.
+    :return:  Nothing
+    """
+    # Shell escape filename for execution #
+    file = shlex.quote(filename)
+
+    try:
+        # Open the temp file in write mode #
+        with open(tmp_file, 'w', encoding='utf-8') as out_file:
+            # Create objdump command process, storing output in open file #
+            with Popen(['objdump', '-M', 'intel', '-D', file], stdout=out_file,
+                       stderr=out_file) as command:
+                try:
+                    command.communicate()
+
+                # When process completes or is timed out #
+                except (TimeoutExpired, ValueError):
+                    command.kill()
+                    command.communicate()
+
+    # If error occurs during file operation #
+    except (IOError, OSError) as io_err:
+        print_err(f'Error occurred writing {file} objdump to {tmp_file}: {io_err}', 2)
+        sys.exit(2)
+
+
+def print_err(msg: str, seconds: int):
+    """
+    Prints a timed error message via stderr.
+
+    :param msg:  The error message to be displayed.
+    :param seconds:   The time interval that the error message will be displayed.
+    :return:  Nothing
+    """
+    print(f'\n* [ERROR]: {msg} *\n', file=sys.stderr)
+    time.sleep(seconds)
+
+
+def prompt_user(re_exe) -> str:
+    """
+    Prompt the user for input, handle errors accordingly.
+
+    :param re_exe:  The regular expression match format.
+    :return:  Validated user input.
+    """
+    while True:
+        # Clear the display #
+        os.system('clear')
+        # Prompt user for input #
+        prompt = input('invalid or no args provided .. enter file to grab bytes\n')
+
+        # If input validation regex fails #
+        if not re.search(re_exe, prompt):
+            print_err('Improper input .. try again', 2)
+            time.sleep(2)
+            continue
+
+        return prompt
+
+
+def main():
+    """
+    Take input file, run in objdump, and parse output into shellcode.
+
+    :return:  Nothing
+    """
+    # Compile path regex #
+    re_exe = re.compile(r'[a-zA-Z\d_\"\' .,\-]{1,20}')
+
+    # If an arg was passed in #
+    if len(sys.argv) > 1:
+        # Check if arg was passed in #
+        arg_check = re.search(re_exe, sys.argv[1])
+
+        # If regex did not match #
+        if not arg_check:
+            # Prompt the user for input #
+            filename = prompt_user(re_exe)
+        else:
+            # Set the passed in arg as filename #
+            filename = sys.argv[1]
+    # If no args were passed in #
+    elif len(sys.argv) == 1:
+        # Prompt the user for binary tp execute on #
+        filename = prompt_user(re_exe)
+    # If unexpected error occurs parsing args #
+    else:
+        print_err('Unknown error occurred on program startup', 2)
+        # Exit program on error #
+        sys.exit(1)
+
+    out_path = '/tmp/'
+    # Set temp file name for pre-parsing #
+    tmp_file = f'{out_path}{filename}.txt'
+
+    # Run objdump utility on binary file and save to tmp file #
+    objdump_run(filename, tmp_file)
+
+    # Compile regular expression for byte formatting #
+    re_byte = re.compile(r'\s([a-f\d]{2}\s){1,7}')
+    re_space_strip = re.compile(r'\s[^\S\r\n]')
+    re_raw_byte = re.compile(r'(?:\s|\t)')
+    shellcode = ''
+
+    try:
+        # Open the temporary file in read mode #
+        with open(tmp_file, 'r', encoding='utf-8') as re_file:
+            # Iterate through file line by line #
+            for line in re_file:
+                # Attempt to match bytes in line #
+                byte_grab = re.search(re_byte, line)
+                # If regex matches #
+                if byte_grab:
+                    # Strip whitespace from match #
+                    strip = re.sub(re_space_strip, r'', byte_grab.group(0))
+                    # Format raw byte like \xXX #
+                    raw_byte = re.sub(re_raw_byte, r'\\x', strip)
+                    # Append raw byte to shellcode string #
+                    shellcode += raw_byte[:-2]
+
+        print(f'Shellcode: {shellcode}')
+        # Unlink the temporary parsing file #
+        os.remove(f'{out_path}{filename}.txt')
+
+    # If error occurs during file operation #
+    except (IOError, OSError) as io_err:
+        print_err(f'Error occurred writing shellcode to {out_path}{filename}: {io_err}', 2)
+        sys.exit(3)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
